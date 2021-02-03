1660497c518a2958ef9e5d1db86217dd5f59b4cd
eisenbahnplatte <fabiangoetz@hotmail.de>
2021-02-03 13:25:42 +0100

    fixed img not a uri issue
    updated angular

--- src/main/resources/static/js/wof.js
+++ src/main/resources/static/js/wof.js
@@ -1,6 +1,91 @@
 var app = angular.module('wofApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
-app.controller('webIdController', function($scope, $http) {
+
+
+app.controller('webIdController', function($scope, $http, $filter) {
     $http.get("/webids.json").then(function(response) {
         $scope.webids = response.data.webIds;
     });
+
+    $scope.items = [1,2,3,4,5];
+
+    $scope.filteredArray =  $filter('unique')($scope.webids,'gender');
+
+    $scope.selected = [1];
+
+    $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+            list.splice(idx, 1);
+        }
+        else {
+            list.push(item);
+        }
+    };
+
+    $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+    };
+
+    $scope.isIndeterminate = function() {
+        return ($scope.selected.length !== 0 &&
+            $scope.selected.length !== $scope.items.length);
+    };
+
+    $scope.isChecked = function() {
+        return $scope.selected.length === $scope.items.length;
+    };
+
+    $scope.toggleAll = function() {
+        if ($scope.selected.length === $scope.items.length) {
+            $scope.selected = [];
+        } else if ($scope.selected.length === 0 || $scope.selected.length > 0) {
+            $scope.selected = $scope.items.slice(0);
+        }
+    };
+});
+
+/**
+ * Filters out all duplicate items from an array by checking the specified key
+ * @param [key] {string} the name of the attribute of each object to compare for uniqueness
+ if the key is empty, the entire object will be compared
+ if the key === false then no filtering will be performed
+ * @return {array}
+ */
+angular.module('wofApp').filter('unique', function () {
+
+    return function (items, filterOn) {
+
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var hashCheck = {}, newItems = [];
+
+            var extractValueToCompare = function (item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function (item) {
+                var valueToCheck, isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
 });
