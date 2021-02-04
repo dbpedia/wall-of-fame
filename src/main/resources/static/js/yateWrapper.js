e8a05b4027530421a022ca7e7f3e81874cb3f30a
eisenbahnplatte <fabiangoetz@hotmail.de>
2021-02-04 16:23:51 +0100

    started to solve yate cache problem

---
+++ src/main/resources/static/js/yateWrapper.js
@@ -0,0 +1,44 @@
+var app = angular.module('yateApp', []);
+
+function YasqeTextController($scope, $element) {
+    var ctrl = this;
+    ctrl.textField = $element.find('textarea');
+
+    ctrl.$scope = $scope;  ctrl.$onInit = function() {
+        ctrl.yasqe = YATE.fromTextArea(ctrl.textField[0], {
+            lineNumbers: true,
+            viewportMargin: Infinity,
+            readOnly: ctrl.readOnly,
+            autorefresh: true
+        });
+
+        ctrl.yasqe.on('change', function() {
+            ctrl.query = ctrl.yasqe.getValue();
+            ctrl.valid = !ctrl.yasqe.queryValid;
+            if(!$scope.$root.$$phase) {
+                ctrl.$scope.$apply();
+            }
+        });
+
+        ctrl.yasqe.setValue(ctrl.query);
+    }
+
+    ctrl.$doCheck = function() {
+        if(ctrl.yasqe != undefined && ctrl.yasqe.getValue() != ctrl.query) {
+            ctrl.yasqe.setValue(ctrl.query)
+        }
+
+        setTimeout(function() {
+            ctrl.yasqe.refresh();
+        }, 10);
+    }
+}
+
+app.component('yateText', {
+    templateUrl: '../../templates/yate-text.html',
+    controller: [ '$scope', '$element', YasqeTextController ],
+    bindings: {
+        query: '=',
+        readOnly: '<'
+    }
+});
